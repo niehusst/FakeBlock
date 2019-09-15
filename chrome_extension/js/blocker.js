ce0172e274168a1b9c37bf0a548093297ec831d7
niehusst <liamniehusstaab@gmail.com>
2019-09-15 16:21:02 +0200

    initial blocked post message contents and styling

--- chrome_extension/js/blocker.js
+++ chrome_extension/js/blocker.js
@@ -1,1 +1,22 @@
 // js for post blocker
+
+//TODO determine which image/text to use based on response from API
+var blocker_html = `
+<div class="bg_filler">
+	<div class="content_houser">
+		<img class="block_img" src="../icons/fbWarning.png" alt="Blocked">
+		<p class="block_txt">
+			This post has been determined to contain fake news.
+		</p>
+	</div>
+	<div class="btn_houser">
+		<button class="read_btn">
+			Read Anyway
+		</button>
+	</div>
+</div>`
+
+// inject blocker html
+function(targetPost) {
+
+}
