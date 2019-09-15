46daf473e9e17d64f916096f6bb45d670d461bee
niehusst <liamniehusstaab@gmail.com>
2019-09-15 13:41:37 +0200

    js to remove all post contents live as loaded into stream. Finicky?

--- chrome_extension/js/content.js
+++ chrome_extension/js/content.js
@@ -12,39 +12,17 @@ $(function() {
 	console.log("right before live query");
 	// run a 'live query' on each post as it's added to DOM
 	// TODO: use jquery .each to call hidePostContents on ALL matching elements (use with on somehow?)
-	$('div[data-testid="fbfeed_story"]').on('DOMNodeInserted', hidePostContents());
-	
+	$(document).on('DOMNodeInserted', 'div[data-testid="fbfeed_story"]', function() {
+		console.log("removing everything");
+
+		// remove post text
+		$(this).find('div[data-testid="post_message"]').toggle(false); // toggle true to bring back
+		// remove post images
+		$(this).find('a[data-render-location="newsstand"]').toggle(false);
+		// remove post videos
+		$(this).find('div.mtm').toggle(false);
+		
+		// TODO add marking attribute for easy toggling when plugin deactivated
+	});
 });
 
-function hidePostContents() {
-	console.log("inside callback");
-	//each post has a unique id="hyperfeed_story_id_5d7ce8b3d8d618d30086220"
-	//post is enclosed in div: data-testid="fbfeed_story"
-
-	// post text enclosed in div: data-testid="post_message"
-
-	// image may be selectable by following attribute?
-	//   a: data-render-location="newsstand"
-	//   div: class="uiScaledImageContainer"
-
-	// video may be selectable (where accompanying text was determined bad) with
-	//   div: class="mtm"  ???
-	console.log(this);
-	console.log($(this));
-
-	var postText = $(this).find( $('div[data-testid="post_message"]') );//.toggle(false);
-
-	var postImage = $(this).find('a[data-render-location="newsstand"]').toggle(false);
-
-	var postVid = $(this).find('div.mtm').toggle(false);
-
-	console.log(postText);
-	console.log(postVid);
-	console.log(postImage);
-	
-	console.log("printed info?");
-	postText.style.display = 'none';
-	//document.querySelector('div[data-testid="fbfeed_story"] div[data-testid="post_message"]').style.display = 'none';
-
-	// TODO add marking attribute for easy toggling when plugin deactivated
-}
