e848a808c5b20de5cc0dc70c76433590953c31c0
niehusst <liamniehusstaab@gmail.com>
2019-09-16 16:31:17 +0200

    selective post blocking functional

--- chrome_extension/js/content.js
+++ chrome_extension/js/content.js
@@ -7,11 +7,14 @@ has control over changing the DOM, but cant use chrome apis
 chrome.runtime.sendMessage({todo: "showPageAction"});
 
 
-// inject blocker html
+/** 
+ * Inject blocker html before the comments html
+ *
+ * @param targetPost - an object containing the target post element
+ */
 function injectBlock(targetPost) {
-	console.log("content inject");
 
-	//TODO determine which image/text to use based on response from API
+	//TODO determine which image/text to use based on response from API?
 	// construct programatically instead?
 	var blocker_html = `
 	<div class="bg_filler">
@@ -34,21 +37,37 @@ function injectBlock(targetPost) {
 	//https://i.imgur.com/zPuiLgy.png
 
 	// inject html before comments div
+	// TODO make it insert after profile head instead of before comments? not every post has comments (reposts are like 2 posts)
 	var mark = targetPost.children(':first-child').children(':first-child').children(':first-child').next().children(':first-child').next();
 	$(blocker_html).insertBefore(mark);
 }
 
-// remove post contents
+/** 
+ * Remove contents (image, vid, text) from targetPost
+ *
+ * @param targetPost - an object containing the target post element
+ */
 function removeContents(targetPost) {
 	console.log("removing contents");
 	// remove post text (set display='none')
 	targetPost.find('div[data-testid="post_message"]').toggle(false); // toggle true to bring back
 	// remove post images
-	//targetPost.find('a[data-render-location="newsstand"]').toggle(false);
+	targetPost.find('a[data-render-location="newsstand"]').toggle(false);
 	// remove post videos
 	targetPost.find('div.mtm').toggle(false);
 	
 	// TODO add marking attribute for easy toggling when plugin deactivated
+}
+
+/** 
+ * Determine if a post is fake news or not by calling APIs
+ *
+ * @param targetPost - an object containing the target post element
+ * @return - Boolean, TODO: change this to include information about which tool determined fake?
+ */
+function isFakeNews(targetPost) {
+	//TODO: call APIs
+	return Math.round(Math.random());
 }
 
 // take action on all facebook posts
@@ -56,12 +75,19 @@ $(function() {
 	console.log("right before live query");
 	// run a 'live query' on each post as it's added to DOM
 	// TODO: sometimes misses first post?
+	var visitedPosts = {};
 	$(document).on('DOMNodeInserted', 'div[data-testid="fbfeed_story"]', function() {
+		// dont run on posts that have already been examined
+		if (!($(this).attr('id') in visitedPosts)) {
 
-		removeContents($(this));
-
-		injectBlock($(this));
-		console.log('calls complete');
+			// mark post as looked at
+			visitedPosts[$(this).attr('id')] = true;
+			
+			if (isFakeNews($(this))) {
+				removeContents($(this));
+				injectBlock($(this));
+			}
+		}
 	});
 });
 
