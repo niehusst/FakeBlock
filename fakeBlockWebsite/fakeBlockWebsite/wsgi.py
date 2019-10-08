6fd377068b506b0641e214d8f3d478de93c0d870
niehusst <liamniehusstaab@gmail.com>
2019-10-09 00:28:54 +0200

    started up django proj for API

---
+++ fakeBlockWebsite/fakeBlockWebsite/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fakeBlockWebsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fakeBlockWebsite.settings')
+
+application = get_wsgi_application()
