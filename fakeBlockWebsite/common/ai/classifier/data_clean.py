c810a35d01487e0cc5d6d4e61e79d8eda07d58ac
niehusst <liamniehusstaab@gmail.com>
2019-12-06 23:00:49 +0100

    implemented even sample size between classes

--- fakeBlockWebsite/common/ai/classifier/data_clean.py
+++ fakeBlockWebsite/common/ai/classifier/data_clean.py
@@ -28,27 +28,34 @@ pf_real = pd.read_csv(pf_real_fname)
 
 print("[INFO] data has {} gossipcop headlines and {} politifact headlines".format(len(gc_fake)+len(gc_real), len(pf_real)+len(pf_fake)))
 
+
 #create truth columns
 print("[INFO] Cleaning data frames and merging")
-gc_fake['fake'] = True
-gc_real['fake'] = False
-pf_fake['fake'] = True
-pf_real['fake'] = False
+gc_fake['fake'] = 1
+gc_real['fake'] = 0
+pf_fake['fake'] = 1
+pf_real['fake'] = 0
+#TODO: use one hot for real/fake? then get probability for sure
 
 #merge dataframes
 news_df = pd.concat([gc_real, gc_fake, pf_fake, pf_real])
+#fix concatted index columns
+news_df.index = list(range(len(news_df)))
 
 #delete unused cols
 cols_to_delete = ['id', 'news_url', 'tweet_ids']
-for col in cols_to_delete:
-	del news_df[col]
+news_df = news_df.drop(labels=cols_to_delete, axis='columns')
 
-
+#balance data set to have equal sample sizes between binary classes
+n_true = sum(news_df.fake)
+n_false = len(news_df.fake) - n_true
+remove_instances = abs(n_false - n_true)
+news_df = news_df.drop(news_df[news_df.fake==0].sample(n=remove_instances).index)
 
 #basic metrics
 n_true = sum(news_df.fake)
 n_false = len(news_df.fake) - n_true
-print("[INFO] data has {} True instances and {} False instances".format(n_true, n_false))
+print("[INFO] data has {} fake news instances and {} real news instances".format(n_true, n_false))
 
 #save as new single csv file
 print("[INFO] Saving data set to " + data_set_path)
