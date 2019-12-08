c5b550a28d3e6683c448e369477515fefdcbf665
niehusst <liamniehusstaab@gmail.com>
2019-12-08 14:45:57 +0100

    first attempt convolutional DNN

--- fakeBlockWebsite/common/ai/classifier/model.py
+++ fakeBlockWebsite/common/ai/classifier/model.py
@@ -20,32 +20,22 @@ import tqdm
 import tqdm.auto
 tqdm.tqdm = tqdm.auto.tqdm
 
-#svm imports
-from nltk.tokenize import word_tokenize
-from nltk import pos_tag
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from sklearn.preprocessing import LabelEncoder
-from collections import defaultdict
-from nltk.corpus import wordnet as wn
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import model_selection, naive_bayes, svm
-from sklearn.metrics import accuracy_score
-import nltk
-nltk.download('wordnet')
-nltk.download('stopwords')
-
 
 ###             SET MODEL CONFIGURATIONS             ###
 # Data Loading
 CSV_PATH = 'data/fake_news_dataset.csv'
 DATA_BASE_PATH = 'data/'
-test_size_percent = 0.20 #percent of total data reserved for testing
+test_size_percent = 0.20
 
 # Learning
 step_size = 0.001
 BATCH_SIZE = 32
 num_epochs = 1
+
+# Data info
+MAX_SEQUENCE_LENGTH = 200 #max num words in typical FB post
+MAX_NUM_WORDS = 25000 #max that embedder can learn
+EMBEDDING_DIM = 300
 
 # Saving
 shape_path = 'trained_model/model_shape.json'
@@ -67,57 +57,54 @@ data_frame = pd.read_csv(CSV_PATH)
 Construct numpy ndarrays from the loaded csv to use as training
 and testing datasets.
 """ 
-news_titles = [word_tokenize(str(string).lower()) for string in data_frame['title']] #lowering may destroy valuable context data from all caps words?
+news_titles = data_frame['title']
 news_fakeness = data_frame['fake']
 
 # apply preprocessing functions with map
-#svm preproc
-tag_map = defaultdict(lambda : wn.NOUN)
-tag_map['J'] = wn.ADJ
-tag_map['V'] = wn.VERB
-tag_map['R'] = wn.ADV
+tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+tokenizer.fit_on_texts(news_titles)
+sequences = tokenizer.texts_to_sequences(news_titles)
 
-for index,entry in enumerate(news_titles):
-    # Declaring Empty List to store the words that follow the rules for this step
-    Final_words = []
-    # Initializing WordNetLemmatizer()
-    word_Lemmatized = WordNetLemmatizer()
-    # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
-    for word, tag in pos_tag(entry):
-        # Below condition is to check for Stop words and consider only alphabets
-        if word not in stopwords.words('english') and word.isalpha():
-            word_Final = word_Lemmatized.lemmatize(word,tag_map[tag[0]])
-            Final_words.append(word_Final)
-    # The final processed set of words for each iteration will be stored in 'text_final'
-    data_frame.loc[index,'text_final'] = str(Final_words)
-
+word_index = tokenizer.word_index
+num_words = min(MAX_NUM_WORDS, len(word_index)) + 1
+data = pad_sequences(sequences, 
+                     maxlen=MAX_SEQUENCE_LENGTH, 
+                     padding='pre', 
+                     truncating='pre')
 
 # reshape input data TODO?
 
 # split the preprocessed data into train and test
 train_titles, test_titles, train_fake, test_fake = \
-  train_test_split(data_frame['text_final'], news_fakeness, test_size=test_size_percent, random_state=42)
+  train_test_split(data, news_fakeness, test_size=test_size_percent, random_state=42)
 
 num_train_examples = len(train_titles)
 num_test_examples = len(test_titles)
 
-#encode for SVM
-encoder = LabelEncoder()
-train_fake = encoder.fit_transform(train_fake)
-test_fake = encoder.fit_transform(test_fake)
-
-Tfidf_vect = TfidfVectorizer(max_features=5000)
-Tfidf_vect.fit(data_frame['text_final'])
-print(train_titles)
-Train_titles_Tfidf = Tfidf_vect.transform(train_titles)
-Test_titles_Tfidf = Tfidf_vect.transform(test_titles)
-#print(Tfidf_vect.vocabulary_)
 """
-TODO: Create generator for feeding the training data to the model
-in batches?
+Create generator for feeding the training data to the model
+in batches
 """
+#TODO fix this func
+def load_data(train_data, train_labels, idx, batch_size):
+    start = idx * batch_size
+    end = start + batch_size
+    x = train_data[start:end]
+    y = train_labels[start:end]
+    return (np.array(x), np.array(y))
+
+def generator(train_data, train_labels, batch_size, steps):
+    idx=1
+    while True: 
+        yield load_data(train_data, train_labels, idx-1, batch_size)
+        
+        if idx < steps:
+            idx += 1
+        else:
+            idx = 1
 
 
+print("Data processing complete\n")
 
 ###            DEFINITION OF MODEL SHAPE             ###
 """
@@ -149,52 +136,23 @@ CLASSIC TYPES OF PREPROC
 -> word2vec
 
 https://www.toptal.com/machine-learning/nlp-tutorial-text-classification
-embedding
-conv1d
-flatten
-dense
 // ^ this performed worse than SVM
 
 
-
-
-Model definition TODO: ....
-
 most fb posts wont be longer than 200 chars???
 """
-
-# Attempt at SVM model
-
-def evaluate_model(predict_fun, X_train, y_train, X_test, y_test):
-    '''
-    evaluate the model, both training and testing errors are reported
-    '''
-    # training error
-    y_predict_train = predict_fun(X_train)
-    train_acc = accuracy_score(y_train,y_predict_train)
-    
-    # testing error
-    y_predict_test = predict_fun(X_test)
-    test_acc = accuracy_score(y_test,y_predict_test)
-    
-    return train_acc, test_acc
-
-
-#get generic svm model
-model = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
-model.fit(Train_titles_Tfidf,train_fake)
-
-
-# evaluate model
-train_acc, test_acc = evaluate_model(model.predict, Train_titles_Tfidf, train_fake, Test_titles_Tfidf, test_fake)
-print("Training Accuracy: {:.2f}%".format(train_acc*100))
-print("Testing Accuracy: {:.2f}%".format(test_acc*100))
-
-
-"""
 model = tf.keras.Sequential([
-
-    tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
+    # part 1: word and sequence processing
+    tf.keras.layers.Embedding(num_words,
+                         EMBEDDING_DIM, 
+                         input_length=MAX_SEQUENCE_LENGTH,
+                         trainable=True),
+    tf.keras.layers.Conv1D(128, 5, activation='relu'),
+    tf.keras.layers.GlobalMaxPooling1D(),
+        
+    # part 2: classification
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
     # 1 output: predict classify input as fake or not
 ])
 
@@ -225,11 +183,11 @@ callbacks.append(tf.keras.callbacks.TensorBoard(log_dir='tb_logs/{}'.format(time
 print('Fitting the model\n')
 # start training the model using the data generator and the configurations
 # specified at the top of the file
-model.fit(train_titles, train_fake, #TODO get rid of generator?
-    batch_size=BATCH_SIZE,
+steps_in_epoch = (num_train_examples // BATCH_SIZE)
+model.fit_generator(generator(train_titles, train_fake, batch_size=BATCH_SIZE, steps=steps_in_epoch),
     callbacks=callbacks, 
     epochs=num_epochs,
-    steps_per_epoch=(num_train_examples // BATCH_SIZE))
+    steps_per_epoch=steps_in_epoch)
 
 
 
@@ -251,4 +209,4 @@ with open(shape_path, "w") as json_file:
 # serialize weights to HDF5
 model.save_weights(weight_path)
 print("Saved model to disk")
-"""
+
