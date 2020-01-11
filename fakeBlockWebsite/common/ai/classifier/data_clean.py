287c9ecab068a42b8693f14313103ed58d98c9f0
niehusst <liamniehusstaab@gmail.com>
2020-01-11 17:49:20 +0100

    updated cleaning for new data

--- fakeBlockWebsite/common/ai/classifier/data_clean.py
+++ fakeBlockWebsite/common/ai/classifier/data_clean.py
@@ -2,24 +2,27 @@
 import os
 import pandas as pd
 import urllib.request
-
+#TODO ack data in README
 #data cleaning script
 url_base = 'https://raw.githubusercontent.com/KaiDMML/FakeNewsNet/master/dataset/'
-url2 = 'https://raw.githubusercontent.com/lutzhamel/fake-news/master/data/fake_or_real_news.csv'
+url_combined_data = 'https://raw.githubusercontent.com/lutzhamel/fake-news/master/data/fake_or_real_news.csv'
 gc_fake_fname = 'gc_fake.csv'
 gc_real_fname = 'gc_real.csv'
 pf_fake_fname = 'pf_fake.csv'
 pf_real_fname = 'pf_real.csv'
 real_fake_fname = 'real_fake.csv'
+non_news_fname = 'data/non_news.csv'
+fake_news_fname = 'data/fake.csv'
+
 data_set_path = 'data/fake_news_dataset.csv'
 
-#download datasets from FakeNewsNet github
+#download datasets from github
 print("[INFO] Downloading datasets from github...", end="", flush=True)
 urllib.request.urlretrieve(url_base + 'gossipcop_fake.csv', gc_fake_fname)
 urllib.request.urlretrieve(url_base + 'gossipcop_real.csv', gc_real_fname)
 urllib.request.urlretrieve(url_base + 'politifact_fake.csv', pf_fake_fname)
 urllib.request.urlretrieve(url_base + 'politifact_real.csv', pf_real_fname)
-urllib.request.urlretrieve(url2, real_fake_fname)
+urllib.request.urlretrieve(url_combined_data, real_fake_fname)
 print("[DONE]")
 
 #get files as dataframes
@@ -29,36 +32,52 @@ gc_real = pd.read_csv(gc_real_fname)
 pf_fake = pd.read_csv(pf_fake_fname)
 pf_real = pd.read_csv(pf_real_fname)
 real_fake_df = pd.read_csv(real_fake_fname)
+non_news = pd.read_csv(non_news_fname, engine='python') #from https://www.kaggle.com/c/twitter-sentiment-analysis2/data
+fake_news = pd.read_csv(fake_news_fname) #from https://www.kaggle.com/mrisdal/fake-news
 
+
+print("[INFO] Cleaning data frames and merging")
+#clean up fake news dataset; delete unneeded cols and repeat/non-english rows
+fake_news.drop(fake_news[fake_news.language != 'english'].index, inplace=True)
+fake_news['title'] = fake_news['thread_title']
+fake_news = fake_news[['title']]
+fake_news.drop_duplicates(inplace=True)
+
+#strip white-space off non-news twitter post strings
+non_news.applymap(lambda x: x.strip() if isinstance(x, str) else x)
 
 #create truth columns. Fake news has 1 (True) in it's `fake` column
-print("[INFO] Cleaning data frames and merging")
 gc_fake['fake'] = 1
 gc_real['fake'] = 0
 pf_fake['fake'] = 1
 pf_real['fake'] = 0
+fake_news['fake'] = 1
+non_news['fake'] = 0
 real_fake_df['fake'] = list(map(int, real_fake_df['label']=='FAKE'))
 
+#get all text to analyze under the same column name
+non_news['title'] = non_news['SentimentText']
+
 #merge dataframes
-news_df = pd.concat([gc_real, gc_fake, pf_fake, pf_real, real_fake_df])
+news_df = pd.concat([gc_real, gc_fake, pf_fake, pf_real, real_fake_df, non_news, fake_news])
 #fix concatted index columns
 news_df.index = list(range(len(news_df)))
 
 
 #delete unused cols
-cols_to_delete = ['id', 'news_url', 'tweet_ids', 'label', 'text']
-news_df = news_df.drop(labels=cols_to_delete, axis='columns')
+news_df = news_df[['title', 'fake']]
 
 #balance data set to have equal sample sizes between binary classes
-n_true = sum(news_df.fake)
-n_false = len(news_df.fake) - n_true
-remove_instances = abs(n_false - n_true)
-news_df = news_df.drop(news_df[news_df.fake==0].sample(n=remove_instances).index)
+num_fake = sum(news_df.fake)
+num_real = len(news_df.fake) - num_fake
+remove_instances = abs(num_real - num_fake)
+#remove randomly from the class that has more occurances in the dataset
+news_df = news_df.drop(news_df[news_df.fake==int(num_fake>num_real)].sample(n=remove_instances).index)
 
 #basic metrics
-n_true = sum(news_df.fake)
-n_false = len(news_df.fake) - n_true
-print("[INFO] {} data points collected. {} Real, {} Fake".format(len(news_df), n_true, n_false))
+n_fake = sum(news_df.fake)
+n_real = len(news_df.fake) - n_fake
+print("[INFO] {} data points collected. {} Real, {} Fake".format(len(news_df), n_real, n_fake))
 
 #save as new single csv file
 print("[INFO] Saving data set to " + data_set_path)
