037fc4989b3095a929a62ff83c02f1be4d246240
niehusst <liamniehusstaab@gmail.com>
2020-02-02 20:18:54 -0600

    added documentation

--- fakeBlockWebsite/fakeBlockDemo/views/views.py
+++ fakeBlockWebsite/fakeBlockDemo/views/views.py
@@ -1,10 +1,9 @@
 from django.template.response import TemplateResponse
 from django.http import HttpResponse
-#from django.http import JsonResponse
-#from rest_framework.reverse import reverse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.status import HTTP_400_BAD_REQUEST
 from django.views import View
+
+from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.decorators import api_view
 
 
@@ -25,6 +24,11 @@ class DemoIndex(View):
 class AboutPage(View):
 	def get(self, request):
 		"""
+		Return the HTML template for the about page.
+
+		@param request - a rest_framework.request.Request object. There are
+						no body parameters.
+		@return - a TemplateResponse object that loads the about HTML template
 		"""
 		return TemplateResponse(request, 'about.html', {})
 
@@ -32,5 +36,10 @@ class AboutPage(View):
 class ApiDocsPage(View):
 	def get(self, request):
 		"""
+		Return the HTML template for the api docs page.
+
+		@param request - a rest_framework.request.Request object. There are
+						no body parameters.
+		@return - a TemplateResponse object that loads the api HTML template
 		"""
 		return TemplateResponse(request, 'api.html', {})
