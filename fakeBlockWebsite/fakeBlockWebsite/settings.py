e0d5060482620d1992a88a603b6a6997f11a2873
niehusst <liamniehusstaab@gmail.com>
2020-02-05 12:09:00 -0600

    added custom error pages

--- fakeBlockWebsite/fakeBlockWebsite/settings.py
+++ fakeBlockWebsite/fakeBlockWebsite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # TODO SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
